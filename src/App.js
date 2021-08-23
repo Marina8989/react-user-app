439b355f339140f7d60f8e1b537f0fe086a6062d
Marina8989 <marina.mill209@gmail.com>
2021-08-22 19:08:48 -0700

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const UserItem = (props) => {
+   return (
+       <div>
+           <li>
+            {props.item.value} 
+            <button onClick={() => props.handleDelete(props.item)}>X</button>
+            <button onClick={() => props.handleUpdate(props.item)}>Update</button>
+            </li>
+       </div>
+   )
 }
+const UserList = (props) => {
+   return (
+       <ul>
+          {props.user.map(item => <UserItem key={item} item={item} handleDelete={props.handleDelete} handleUpdate={props.handleUpdate}/>)} 
+       </ul>
+   )
+}
+
+class Form extends React.Component{
+    state={
+        inputValue: ''
+    }
+    handleChange = (e) => {
+       this.setState({inputValue: e.target.value});
+    }
+    handleSubmit = (e) => {
+      e.preventDefault();
+      const newValue = this.state.inputValue;
+      console.log(newValue)
+      this.setState({inputValue: ''});
+      this.props.handleSubmit(newValue);
+    }
+    handleUpdate = (item) => {
+      console.log(item);
+      console.log(this.state)
+    }
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input value={this.state.inputValue} onChange={this.handleChange} handleUpdate={this.handleUpdate}/>
+            </form>
+        )
+    }
+}
+class App extends React.Component {
+    state={
+        user: []
+    }
+    handleSubmit = (value) => {
+        const item = {
+            id: `${Math.floor(Math.random() * 20)}`,
+            value,
+        }
+        const newUsers = [...this.state.user, item]
+        this.setState({user: newUsers});
+        console.log(this.state.user)
+    }
+
+    handleDelete = (element) => {
+      const deleteItem = this.state.user.filter(item => element.id !== item.id)
+      console.log(deleteItem);
+      this.setState({user: deleteItem});
+    }
+    
+    render() {
+        return(
+            <div>
+            <Form handleSubmit={this.handleSubmit}/>
+            <UserList user={this.state.user} handleDelete={this.handleDelete} />
+            </div>
+        )
+    }
+} 
+
 
 export default App;
